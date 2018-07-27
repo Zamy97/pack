1cbe99c430702dd891460536000385fcaff9a6a2
Aktar Zaman <Auz.zamy@gmail.com>
2018-07-27 14:39:32 -0700

    Initial Commit

---
+++ PCK Sggtion/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PCK Sggtion
+//
+//  Created by Akhtar zaman on 7/27/18.
+//  Copyright © 2018 Akhtar zaman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

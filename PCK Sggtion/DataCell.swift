8b596e5ff6e2c9340b8d6b79085ff80d872d0f7c
Aktar Zaman <Auz.zamy@gmail.com>
2018-07-28 19:07:22 -0700

    Still tryna figure out the table!

---
+++ PCK Sggtion/DataCell.swift
@@ -0,0 +1,20 @@
+//
+//  DataCell.swift
+//  PCK Sggtion
+//
+//  Created by Akhtar zaman on 7/28/18.
+//  Copyright © 2018 Akhtar zaman. All rights reserved.
+//
+
+import UIKit
+
+class DateCell: UICollectionViewCell {
+    @IBOutlet weak var datelbl: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}

3a0e34bc4a0d314367f05be928605d2edecc4f61
Aktar Zaman <Auz.zamy@gmail.com>
2018-07-31 14:51:54 -0700

    Moved the search bar to the second view controller from view controller

--- PCK Sggtion/SecondViewViewController.swift
+++ PCK Sggtion/SecondViewViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-import CVCalendar
+//import CVCalendar
 import GooglePlaces
 import GoogleMaps
 
@@ -22,8 +22,31 @@ class SecondViewViewController: UIViewController {
     let dateFormatter1 = DateFormatter()
     let dateFormatter2 = DateFormatter()
     
+    var locationManager = CLLocationManager()
+    
+    
+    let txtFieldSearch: UITextField = {
+        let tf=UITextField()
+        tf.borderStyle = .roundedRect
+        tf.backgroundColor = .white
+        tf.layer.borderColor = UIColor.darkGray.cgColor
+        tf.placeholder="Search for a location"
+        tf.translatesAutoresizingMaskIntoConstraints=false
+        return tf
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        locationManager.startMonitoringSignificantLocationChanges()
+        
+        setupViews()
+        txtFieldSearch.delegate = self
+        
         
         datePicker1 = UIDatePicker()
         datePicker1.datePickerMode = .date
@@ -48,8 +71,8 @@ class SecondViewViewController: UIViewController {
     @objc func dateChanged(datePicker: UIDatePicker) {
         
         
-        dateFormatter1.dateFormat = String("MM/dd/yyyy")
-        dateFormatter2.dateFormat = String("MM/dd/yyyy")
+        dateFormatter1.dateFormat = String("MM-dd-yyyy")
+        dateFormatter2.dateFormat = String("MM-dd-yyyy")
         
         inputTextField.text = String(dateFormatter1.string(from: datePicker1.date))
         outputTextField.text = String(dateFormatter2.string(from: datePicker2.date))
@@ -67,6 +90,76 @@ class SecondViewViewController: UIViewController {
         }
        
     }
+    
   
 
+}
+
+extension SecondViewViewController: CLLocationManagerDelegate, GMSMapViewDelegate, GMSAutocompleteViewControllerDelegate, UITextFieldDelegate {
+    
+    
+    
+    //MARK: textfield
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        let autoCompleteController = GMSAutocompleteViewController()
+        autoCompleteController.delegate = self
+        
+        let filter = GMSAutocompleteFilter()
+        autoCompleteController.autocompleteFilter = filter
+        
+        self.locationManager.startUpdatingLocation()
+        self.present(autoCompleteController, animated: true, completion: nil)
+        return false
+    }
+    
+    // MARK: GOOGLE AUTO COMPLETE DELEGATE
+    func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
+   //     let lat = place.coordinate.latitude
+      //  let long = place.coordinate.longitude
+        
+        txtFieldSearch.text=place.formattedAddress
+        
+        //  chosenPlace = MyPlace(name: place.formattedAddress!, lat: lat, long: long)
+        
+        
+        self.dismiss(animated: true, completion: nil) // dismiss after place selected
+    }
+    
+    func viewController(_ viewController: GMSAutocompleteViewController, didFailAutocompleteWithError error: Error) {
+        print("ERROR AUTO COMPLETE \(error)")
+    }
+    
+    func wasCancelled(_ viewController: GMSAutocompleteViewController) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Error while getting location \(error)")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        locationManager.delegate = nil
+        locationManager.stopUpdatingLocation()
+    }
+    
+    func setupTextField(textField: UITextField, img: UIImage){
+        textField.leftViewMode = UITextFieldViewMode.always
+        let imageView = UIImageView(frame: CGRect(x: 5, y: 5, width: 20, height: 20))
+        imageView.image = img
+        let paddingView = UIView(frame:CGRect(x: 0, y: 0, width: 30, height: 30))
+        paddingView.addSubview(imageView)
+        textField.leftView = paddingView
+    }
+    
+    func setupViews() {
+        
+        self.view.addSubview(txtFieldSearch)
+        txtFieldSearch.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50).isActive=true
+        txtFieldSearch.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 10).isActive=true
+        txtFieldSearch.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -10).isActive=true
+        txtFieldSearch.heightAnchor.constraint(equalToConstant: 35).isActive=true
+        setupTextField(textField: txtFieldSearch, img: #imageLiteral(resourceName: "map_Pin"))
+    }
+ 
 }
--- PCK Sggtion/ViewController.swift
+++ PCK Sggtion/ViewController.swift
@@ -10,100 +10,98 @@ import UIKit
 import GoogleMaps
 import GooglePlaces
 
-class ViewController: UIViewController, CLLocationManagerDelegate, GMSMapViewDelegate, GMSAutocompleteViewControllerDelegate, UITextFieldDelegate {
+class ViewController: UIViewController {
     
-    var locationManager = CLLocationManager()
+ //   var locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = "Home"
-        self.view.backgroundColor = UIColor.white
-        
-        
-        locationManager.delegate = self
-        locationManager.requestWhenInUseAuthorization()
-        locationManager.startUpdatingLocation()
-        locationManager.startMonitoringSignificantLocationChanges()
-        
-        setupViews()
-        txtFieldSearch.delegate = self
+//        self.title = "Home"
+//        self.view.backgroundColor = UIColor.white
+//
+//
+//        locationManager.delegate = self
+//        locationManager.requestWhenInUseAuthorization()
+//        locationManager.startUpdatingLocation()
+//        locationManager.startMonitoringSignificantLocationChanges()
+//
+//        setupViews()
+//        txtFieldSearch.delegate = self
     }
-    
-    //MARK: textfield
-    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
-        let autoCompleteController = GMSAutocompleteViewController()
-        autoCompleteController.delegate = self
-        
-        let filter = GMSAutocompleteFilter()
-        autoCompleteController.autocompleteFilter = filter
-        
-        self.locationManager.startUpdatingLocation()
-        self.present(autoCompleteController, animated: true, completion: nil)
-        return false
-    }
-    
-    // MARK: GOOGLE AUTO COMPLETE DELEGATE
-    func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
-        let lat = place.coordinate.latitude
-        let long = place.coordinate.longitude
-        
-        txtFieldSearch.text=place.formattedAddress
-        
-        //  chosenPlace = MyPlace(name: place.formattedAddress!, lat: lat, long: long)
-        
-        
-        self.dismiss(animated: true, completion: nil) // dismiss after place selected
-    }
-    
-    func viewController(_ viewController: GMSAutocompleteViewController, didFailAutocompleteWithError error: Error) {
-        print("ERROR AUTO COMPLETE \(error)")
-    }
-    
-    func wasCancelled(_ viewController: GMSAutocompleteViewController) {
-        self.dismiss(animated: true, completion: nil)
-    }
-    
-    
-    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        print("Error while getting location \(error)")
-    }
-    
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        locationManager.delegate = nil
-        locationManager.stopUpdatingLocation()
-    }
-    
-    func setupTextField(textField: UITextField, img: UIImage){
-        textField.leftViewMode = UITextFieldViewMode.always
-        let imageView = UIImageView(frame: CGRect(x: 5, y: 5, width: 20, height: 20))
-        imageView.image = img
-        let paddingView = UIView(frame:CGRect(x: 0, y: 0, width: 30, height: 30))
-        paddingView.addSubview(imageView)
-        textField.leftView = paddingView
-    }
-    
-    func setupViews() {
-        
-        self.view.addSubview(txtFieldSearch)
-        txtFieldSearch.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10).isActive=true
-        txtFieldSearch.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 10).isActive=true
-        txtFieldSearch.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -10).isActive=true
-        txtFieldSearch.heightAnchor.constraint(equalToConstant: 35).isActive=true
-        setupTextField(textField: txtFieldSearch, img: #imageLiteral(resourceName: "map_Pin"))
-    }
-    
-    
-    let txtFieldSearch: UITextField = {
-        let tf=UITextField()
-        tf.borderStyle = .roundedRect
-        tf.backgroundColor = .white
-        tf.layer.borderColor = UIColor.darkGray.cgColor
-        tf.placeholder="Search for a location"
-        tf.translatesAutoresizingMaskIntoConstraints=false
-        return tf
-    }()
-    
-    
-    
+//
+//    //MARK: textfield
+//    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+//        let autoCompleteController = GMSAutocompleteViewController()
+//        autoCompleteController.delegate = self
+//
+//        let filter = GMSAutocompleteFilter()
+//        autoCompleteController.autocompleteFilter = filter
+//
+//        self.locationManager.startUpdatingLocation()
+//        self.present(autoCompleteController, animated: true, completion: nil)
+//        return false
+//    }
+//
+//    // MARK: GOOGLE AUTO COMPLETE DELEGATE
+//    func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
+//        let lat = place.coordinate.latitude
+//        let long = place.coordinate.longitude
+//
+//        txtFieldSearch.text=place.formattedAddress
+//
+//        //  chosenPlace = MyPlace(name: place.formattedAddress!, lat: lat, long: long)
+//
+//
+//        self.dismiss(animated: true, completion: nil) // dismiss after place selected
+//    }
+//
+//    func viewController(_ viewController: GMSAutocompleteViewController, didFailAutocompleteWithError error: Error) {
+//        print("ERROR AUTO COMPLETE \(error)")
+//    }
+//
+//    func wasCancelled(_ viewController: GMSAutocompleteViewController) {
+//        self.dismiss(animated: true, completion: nil)
+//    }
+//
+//
+//    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+//        print("Error while getting location \(error)")
+//    }
+//
+//    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+//        locationManager.delegate = nil
+//        locationManager.stopUpdatingLocation()
+//    }
+//
+//    func setupTextField(textField: UITextField, img: UIImage){
+//        textField.leftViewMode = UITextFieldViewMode.always
+//        let imageView = UIImageView(frame: CGRect(x: 5, y: 5, width: 20, height: 20))
+//        imageView.image = img
+//        let paddingView = UIView(frame:CGRect(x: 0, y: 0, width: 30, height: 30))
+//        paddingView.addSubview(imageView)
+//        textField.leftView = paddingView
+//    }
+//
+//    func setupViews() {
+//
+//        self.view.addSubview(txtFieldSearch)
+//        txtFieldSearch.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10).isActive=true
+//        txtFieldSearch.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 10).isActive=true
+//        txtFieldSearch.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -10).isActive=true
+//        txtFieldSearch.heightAnchor.constraint(equalToConstant: 35).isActive=true
+//        setupTextField(textField: txtFieldSearch, img: #imageLiteral(resourceName: "map_Pin"))
+//    }
+//
+//
+//    let txtFieldSearch: UITextField = {
+//        let tf=UITextField()
+//        tf.borderStyle = .roundedRect
+//        tf.backgroundColor = .white
+//        tf.layer.borderColor = UIColor.darkGray.cgColor
+//        tf.placeholder="Search for a location"
+//        tf.translatesAutoresizingMaskIntoConstraints=false
+//        return tf
+//    }()
+   
 }
 
